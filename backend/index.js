ab33120254aacd897bc9d88f3fa5dd57d682a96c
unknown <nabeerasheed@gmail.com>
2024-05-24 12:03:05 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ app.use(cors())
 app.use(express.json())
 mongoose.connect("mongodb://127.0.0.1:27017/todo").then(()=>console.log("DB success"))
 .catch(()=>console.log("DB failed"))
-const Fruit =
-
+const Fruit =mongoose.model("Fruit",{name:String},"fruit")
 
 //const fruit=["Apple","orange"]
 app.get("/fruits",function(req,res){
@@ -18,11 +17,21 @@ app.get("/fruits",function(req,res){
 })
 app.post("/addfruit",function(req,res)
 {
+    
     var newfruit=req.body.newfruit
     const newFruit= new Fruit({
         name:newfruit
     });
-    newFruit.save().then(()=>console.log("savedsuccesfully..."))
+    newFruit.save().then((data)=>{
+        console.log("savedsuccesfully...")
+    res.send(data)})
+})
+app.post("/delfruit",function(req,res){
+    
+    var did=req.body.did
+    Fruit.findByIdAndDelete(did).then(function(ddata){
+        //console.log(ddata)
+    })
 })
 app.listen("5000",function(){
     console.log("server started.....")
